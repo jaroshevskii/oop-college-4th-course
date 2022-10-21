399fb22d496984105c9a7806dae7779cd4ffc85c
jaroshevskii <jaroshevskiisasha@gmail.com>
2022-10-22 02:37:30 +0300

    Finish Swift version

--- practice/practical-work7/Main.swift
+++ practice/practical-work7/Main.swift
@@ -51,7 +51,7 @@ class Transportation {
       case .oversized:
         return "Oversized"
       case .fireHazard:
-        return "Fire hazard"    
+        return "Fire hazard"
       }
     }
 
@@ -86,24 +86,31 @@ class Transportation {
     Swift.print("\(indent)> \(lable)")
     return Transportation(
       cargoType: CargoType.read(nestingLevel: nestingLevel + 1),
-      cargoMass: Double.read(in: 1...100_000, lable: "Cargo mass (kg)", nestingLevel: nestingLevel + 1),
+      cargoMass: Double.read(
+        in: 1...100_000, lable: "Cargo mass (kg)", nestingLevel: nestingLevel + 1
+      ),
       distance: Int.read(in: 1...100_000, lable: "Distance (km)", nestingLevel: nestingLevel + 1)
     )
   }
 
   /// Writes the textual representations of the `Transportation` into the standard output.
   func print(terminator: String = "\n") {
-    Swift.print("""
-    Transportation:
-      Cargo type: \(cargoType.description())
-      Cargo mass: \(cargoMass)
-      Distance:   \(distance)
-    """, terminator: terminator)
+    Swift.print(
+      """
+      Transportation:
+        Cargo type: \(cargoType.description())
+        Cargo mass: \(cargoMass)
+        Distance:   \(distance)
+      """, terminator: terminator)
   }
 }
 
 class Trucks {
-  var transportations = [Transportation]()
+  var transportations: [Transportation]
+
+  init(transportations: [Transportation] = []) {
+    self.transportations = transportations
+  }
 
   func append() {
     transportations.append(Transportation.read(lable: "New transportation"))
@@ -117,56 +124,107 @@ class Trucks {
       transportations.remove(at: index)
     }
   }
-   
+
   /// Writes the textual representations of the `Trucks` into the standard output.
-  func print(terminator: String = "\n") {
+  func print(cargoType: Transportation.CargoType? = nil, terminator: String = "\n") {
+    var isTerminationFound = false
     if transportations.isEmpty {
       Swift.print("Note: Empty", terminator: terminator)
     } else {
       let separator = String(repeating: "-", count: 80)
       for transportation in transportations {
-        Swift.print(separator)
-        transportation.print()
+        if cargoType == nil || cargoType == transportation.cargoType {
+          isTerminationFound = true
+          Swift.print(separator)
+          transportation.print()
+        }
       }
-      Swift.print(separator, terminator: terminator)
+      if isTerminationFound {
+        Swift.print(separator, terminator: terminator)
+      } else {
+        Swift.print("Note: Empty", terminator: terminator)
+      }
     }
+  }
+
+  /// Writes the textual representations of the `Trucks` into the standard output.
+  func print(cargoMass: Double, terminator: String = "\n") {
+    var isTerminationFound = false
+    if transportations.isEmpty {
+      Swift.print("Note: Empty", terminator: terminator)
+    } else {
+      let separator = String(repeating: "-", count: 80)
+      for transportation in transportations {
+        if transportation.cargoMass > cargoMass {
+          isTerminationFound = true
+          Swift.print(separator)
+          transportation.print()
+        }
+      }
+      if isTerminationFound {
+        Swift.print(separator, terminator: terminator)
+      } else {
+        Swift.print("Note: Empty", terminator: terminator)
+      }
+    }
+  }
+
+  func printSortedByTransportationsDistance(terminator: String = "\n") {
+    Trucks(
+      transportations: transportations.sorted { $0.distance > $1.distance }
+    ).print(terminator: terminator)
   }
 }
 
 // MARK: Main
 
-print("""
-     ______                 __
-    /_  __/____ __ __ ____ / /__ ___
-     / /  / __// // // __//  '_/(_-<
-    /_/  /_/   \\_,_/ \\__//_/\\_\\/___/
-""", terminator: "\n\n")
+print(
+  """
+       ______                 __
+      /_  __/____ __ __ ____ / /__ ___
+       / /  / __// // // __//  '_/(_-<
+      /_/  /_/   \\_,_/ \\__//_/\\_\\/___/
+  """, terminator: "\n\n")
 
 var trucks = Trucks()
 
 menu: while true {
-  print("""
-  > Menu:
-      1. Append
-      2. Remove
-      3. Print
-      4. Exit
-  """)
-  let choise = Int.read(in: 0...4)
-  print() // Empty line.
+  print(
+    """
+    > Menu:
+        1. Append
+        2. Remove
+        3. Print
+        4. Print trucks with a given cargo type
+        5. Print sorted trucks by transportation distance
+        6. Print trucks which have a cargo mass greater than the given
+        7. Exit
+    """)
+  let choise = Int.read(in: 0...7)
+  print()  // Empty line.
 
   switch choise {
   case 0:
     print("  Created with ❤️ by jaroshevskii", terminator: "\n\n")
   case 1:
     trucks.append()
-    print() // Empty line.
+    print()  // Empty line.
   case 2:
     trucks.remove()
-    print() // Empty line.
+    print()  // Empty line.
   case 3:
     trucks.print(terminator: "\n\n")
-  case 4: // Exit.
+  case 4:
+    let cargoType = Transportation.CargoType.read()
+    print()  // Empty line.
+    trucks.print(cargoType: cargoType, terminator: "\n\n")
+  case 5:
+    trucks.printSortedByTransportationsDistance(terminator: "\n\n")
+  case 6:
+    let cargoMass = Double.read(in: 1...100_000, lable: "Cargo mass (kg)")
+    print()  // Empty line.
+    trucks.print(cargoMass: cargoMass, terminator: "\n\n")
+  case 7:  // Exit.
     print("  (* ^ ω ^) goodbye", terminator: "\n\n")
     break menu
   default:
